1f3548caf313665d3b5ba30846fcd5b064fc6c91
wjt <wjt128@gmail.com>
2015-11-25 23:08:35 +0800

    开始做 custom filter

---
+++ app/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* App Module */
+
+var phonecatApp = angular.module('myApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/phones', {
+      templateUrl: 'phone-list/phone-list.html',
+      controller: 'PhoneListCtrl'
+    }).
+    when('/phones/:phoneId', {
+      templateUrl: 'phone-detail/phone-detail.html',
+      controller: 'PhoneDetailCtrl'
+    }).
+    otherwise({
+      redirectTo: '/phones'
+    });
+  }]);
