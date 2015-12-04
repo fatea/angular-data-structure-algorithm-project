7b1531f90cdaa884dbb1b7a182aff47265cd4676
wjt <wjt128@gmail.com>
2015-12-04 20:34:36 +0800

    终于把地图每个点的坐标输入完成了...

--- app/maps/Map_for_walking.js
+++ app/maps/Map_for_walking.js
@@ -5,9 +5,43 @@ var Edge = require('./lib/Edge.js');
 function Map() {
   var map = new Graph();
 //map.addVertex(new Vertex("A"));
+  /*
   for (var i = 0; i < 26; i++) {
     map.addVertex(new Vertex(String.fromCharCode(65 + i)));
   }
+  */
+
+  map.addVertex(new Vertex('A', 71, 233));
+  map.addVertex(new Vertex('B',125, 472));
+  map.addVertex(new Vertex('C',146, 593));
+  map.addVertex(new Vertex('D',240, 164));
+  map.addVertex(new Vertex('E',272, 237));
+  map.addVertex(new Vertex('F',272, 323));
+  map.addVertex(new Vertex('G',284, 421));
+  map.addVertex(new Vertex('H',294, 563));
+  map.addVertex(new Vertex('I',407, 76));
+  map.addVertex(new Vertex('J',422, 212));
+  map.addVertex(new Vertex('K',429, 296));
+  map.addVertex(new Vertex('L',437, 397));
+  map.addVertex(new Vertex('M',442, 463));
+  map.addVertex(new Vertex('N',542, 44));
+  map.addVertex(new Vertex('O',577, 148));
+  map.addVertex(new Vertex('P',599, 243));
+  map.addVertex(new Vertex('Q',657, 312));
+  map.addVertex(new Vertex('R',595, 396));
+  map.addVertex(new Vertex('S',720, 376));
+  map.addVertex(new Vertex('T',671, 192));
+  map.addVertex(new Vertex('U',768, 109));
+  map.addVertex(new Vertex('V',875, 24));
+  map.addVertex(new Vertex('W',952, 101));
+  map.addVertex(new Vertex('X',878, 109));
+  map.addVertex(new Vertex('Y',900, 313));
+  map.addVertex(new Vertex('Z',776, 354));
+
+
+
+
+
 
   const auto = 1;
   const walk = 2;
--- app/maps/lib/Vertex.js
+++ app/maps/lib/Vertex.js
@@ -1,12 +1,14 @@
 'use strict';
 class Vertex{
-  constructor(name){
+  constructor(name,x,y){
     if(typeof name != 'undefined'){
       this.name = name;
     }
     this.d = null;
     this.pi = null;
     this.adj = [];
+    this.x = x;
+    this.y = y;
   }
 
 
